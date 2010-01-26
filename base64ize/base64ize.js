e6d0ce111638b7b0c0fdb1e53c2075e3d7dcb73f
a2h <a2h@live.com.au>
2010-01-26 14:59:34 +1100

    Nothing to see here, move along...

--- base64ize/base64ize.js
+++ base64ize/base64ize.js
@@ -4,19 +4,19 @@
  * http://github.com/a2h/js
  * 
  * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Affero General Public License as
+ * it under the terms of the GNU General Public License as
  * published by the Free Software Foundation, either version 3 of the
  * License, or (at your option) any later version.
  * 
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Affero General Public License for more details.
+ * GNU General Public License for more details.
  *
- * Under section 7b of the GNU Affero General Public License you are
+ * Under section 7b of the GNU General Public License you are
  * required to preserve this notice.
  * 
- * You should have received a copy of the GNU Affero General Public License
+ * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
  */
